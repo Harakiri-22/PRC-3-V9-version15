dd2375331fa1556ebbd737d99c050290092b711c
Mishin Dmitiriy <93830209+Harakiri-22@users.noreply.github.com>
2021-12-18 11:23:44 +0300

    Add files via upload

---
+++ PRC-3-V9-version15/DoubleArrayLibrary/Array.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleArrayLibrary
+{
+    public class Array
+    {
+        public Array(int row, int column)
+        {
+            _array = new int[row, column];
+        }
+
+        private readonly Random _random = new Random();
+        private readonly int[,] _array;
+        public int RowLength => _array.GetLength(0);
+        public int ColumnLength => _array.GetLength(1);
+
+        public void Fill(int minimum = 1, int maximum = 5)
+        {
+            for (int i = 0; i < RowLength; i++)
+            {
+                for (int j = 0; j < ColumnLength; j++)
+                {
+                    _array[i, j] = _random.Next(minimum, maximum);
+                }
+            }
+        }
+
+        public void Conservation(string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(RowLength);
+                writer.WriteLine(ColumnLength);
+
+                for (int i = 0; i < RowLength; i++)
+                {
+                    for (int j = 0; j < ColumnLength; j++)
+                    {
+                        writer.WriteLine(_array[i, j]);
+                    }
+                }
+            }
+        }
+
+        public void Opening(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                int rowlength = int.Parse(reader.ReadLine());
+                int columnlength = int.Parse(reader.ReadLine());
+
+                for (int i = 0; i < rowlength; i++)
+                {
+                    for (int j = 0; j < columnlength; j++)
+                    {
+                        _array[i, j] = int.Parse(reader.ReadLine());
+                    }
+                }
+            }
+        }
+
+        public DataTable ToTableData()
+        {
+            var result = new DataTable();
+            for (int i = 0; i < RowLength; i++)
+            {
+                result.Columns.Add("Col" + (i + 1));
+            }
+
+            for (int i = 0; i < ColumnLength; i++)
+            {
+                var row = result.NewRow();
+
+                for (int j = 0; j < ColumnLength; j++)
+                {
+                    row[j] = _array[i, j];
+                }
+
+                result.Rows.Add(row);
+            }
+
+            return result;
+        }
+
+        public int this[int firstindex, int secondindex]
+        {
+            get => _array[firstindex, secondindex];
+            set => _array[firstindex, secondindex] = value;
+        }
+        public void Clear()
+        {
+            Fill(0, 0);
+        }
+    }
+}
---
+++ PRC-3-V9-version15/Lib_9/Calculation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoubleArrayLibrary;
+
+namespace Lib_9
+{
+    public static class Calculation
+    {
+        public static string LastLineNumber(this DoubleArrayLibrary.Array array) // номер последней строки
+        {
+            int[,] elements = new int[array.RowLength, array.ColumnLength];
+            int number, repeat1 = 1, repeat2 = 1, maxi = -1;
+            for (int i = 0; i < array.ColumnLength; i++)
+            {
+                for (int j = 0; j < array.RowLength - 1; j++)
+                {
+                    number = j + 1;
+                    while (number < array.RowLength) // сравниваем элемент с последующим в строке и + счетчик
+                    {
+                        if (elements[i, j] == elements[i, number])
+                        {
+                            repeat1++;
+                        }
+                        number++;
+                    }
+                }
+                if (repeat1 > repeat2) // если в первой строке больше чем в предыдущей запомнить индекс строки 
+                {
+                    repeat2 = repeat1;
+                    maxi = i;
+                }
+                repeat1 = 1;
+            }
+            return maxi.ToString();
+        }
+    }
+}
